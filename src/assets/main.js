0af4267d1c19a137f26a4267d44a4edcffe1d817
jesus castillo <jesus.castillo352@gmail.com>
2022-07-04 13:34:03 -0500

    connecting the new remote with the code

--- src/assets/main.js
+++ src/assets/main.js
@@ -24,7 +24,7 @@ async function fetchData(urlApi) {
       <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
           <h3 class="text-sm text-gray-700">
@@ -36,7 +36,47 @@ async function fetchData(urlApi) {
     `).slice(0,4).join('')}
 
     `;
-  } catch {
-
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
   }
 })();
+
+/* const API = "https://youtube-v31.p.rapidapi.com/search?channelId=OscarBarajas&part=snippet%2Cid&order=date&maxResults=9"
+const content = null || document.getElementById('content');
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd0419c8f6bmsha12157696c68e87p1df6cbjsn95b262bb33ed',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+  const reponse = await fetch(urlApi, options)ñ
+  const data = await reponse.json();
+  return data
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items.map((video) => `<div class="group relative">
+    <div
+      class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+      <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+    </div>
+    <div class="mt-4 flex justify-between">
+      <h3 class="text-sm text-gray-700">
+        <span aria-hidden="true" class="absolute inset-0"></span>
+        ${video.snippet.title}
+      </h3>
+    </div>
+  </div>
+  `).slice(0,4).join("")}
+  `;
+  } catch (error) {
+    console.log(error)
+  }
+})() */
